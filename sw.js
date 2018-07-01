3df93f3b43426f064920fdd9203ad27793190b7b
Stephanie Ekekwe <stephchisom@gmail.com>
2018-07-01 23:17:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open('alc-with-google3.0').then(cache => cache.addAll([
+            './css/bootstrap.min.css',
+            './css/font-awesome.css',
+            './css/style.css',
+            './js/bootstrap.min.js',
+            './js/script.js',
+            './index.html'
+        ]))
+	);
+});
+
+/*fetch the page from the cache. if request doesn't match any in the catch:
+get it from the network, send it to the page and add it to the cache at the same time*/
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request).then(response => response || fetch(event.request).then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+        }))
+	);
+});
